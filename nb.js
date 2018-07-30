e9cbc7932e6888ad99858327726d88d90e112a63
Brian Norberg <BNorberg@hq.genevasi.com>
2018-07-30 18:15:10 -0400

    changed classified object to Map

--- nb.js
+++ nb.js
@@ -81,7 +81,7 @@ setProbabilityOfChordsInLabels();
 
 function classify(chords) {
     var smoothing = 1.01;
-    var classified = {};
+    var classified = new Map();
     console.log(labelProbabilities);
     Object.keys(labelProbabilities).forEach(function(difficulty) {
         var first = labelProbabilities[difficulty] + smoothing;
@@ -91,7 +91,7 @@ function classify(chords) {
                 first = first * (probabilityOfChordInLabel + smoothing);
             }
         });
-        classified[difficulty] = first;
+        classified.set(difficulty, first);
     });
     console.log(classified);
 };
