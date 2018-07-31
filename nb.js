f023acad25ffeff296a285e5f7b297bbdce78502
Brian Norberg <BNorberg@hq.genevasi.com>
2018-07-31 16:14:15 -0400

    initial test - now uses mocha and wish

--- nb.js
+++ nb.js
@@ -99,3 +99,10 @@ function classify(chords) {
 
 classify(['d', 'g', 'e', 'dm']);
 classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
+
+var wish = require('wish');
+describe('this file', function() {
+    it('works', function(){
+        wish(true);
+    });
+});
