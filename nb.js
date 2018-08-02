8387d040048383ff6a597347172aa890d3382b8e
Brian Norberg <BNorberg@hq.genevasi.com>
2018-08-02 16:50:06 -0400

    got rid of probabilityOfChordsInLabels redundant object

--- nb.js
+++ nb.js
@@ -26,10 +26,9 @@ const classifier = {
     labelCounts: new Map(),
     labelProbabilities: new Map(),
     chordCountsInLabels: new Map(),
-    probabilityOfChordsInLabels: new Map(),
     smoothing: 1.01, 
     valueForChordDifficulty: function (difficulty, chord) {
-        const value = this.probabilityOfChordsInLabels.get(difficulty)[chord];
+        const value = this.chordCountsInLabels.get(difficulty)[chord];
         return value ? value + this.smoothing : 1;
     },
     classify: function (chords) {
@@ -77,12 +76,11 @@ function setChordCountsInLabels() {
 }
 
 function setProbabilityOfChordsInLabels() {
-  classifier.probabilityOfChordsInLabels = classifier.chordCountsInLabels;
-  classifier.probabilityOfChordsInLabels.forEach(
+  classifier.chordCountsInLabels.forEach(
     function(_chords, difficulty) {
-      Object.keys(classifier.probabilityOfChordsInLabels.get(difficulty))
+      Object.keys(classifier.chordCountsInLabels.get(difficulty))
         .forEach(function(chord) {
-          classifier.probabilityOfChordsInLabels.get(difficulty)[chord] /= 
+          classifier.chordCountsInLabels.get(difficulty)[chord] /= 
             classifier.songs.length;
         });
     }
