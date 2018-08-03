f6ffb2d44b85879281adda357144339ed8f9151c
Brian Norberg <BNorberg@hq.genevasi.com>
2018-08-03 11:22:18 -0400

    added addSong function to classifier with rest parameter syntax

--- nb.js
+++ nb.js
@@ -24,6 +24,9 @@ class Classifier {
     this.labelProbabilities = new Map();
     this.smoothing = 1.01;
   }
+  addSong(...songParams) {
+    this.songList.addSong(...songParams);
+  }
   chordCountForDifficulty(difficulty, testChord) {
     return this.songList.songs.reduce((counter, song) => {
       if (song.difficulty === difficulty) {
@@ -76,16 +79,16 @@ const wish = require('wish');
 
 describe('the file', () => { 
   const classifier = new Classifier();
-  classifier.songList.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0);
-  classifier.songList.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0);
-  classifier.songList.addSong('tooManyCooks', ['c', 'g', 'f'], 0);
-  classifier.songList.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1);
-  classifier.songList.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1);
-  classifier.songList.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], 1);
-  classifier.songList.addSong('paperBag',
+  classifier.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0);
+  classifier.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0);
+  classifier.addSong('tooManyCooks', ['c', 'g', 'f'], 0);
+  classifier.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1);
+  classifier.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1);
+  classifier.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], 1);
+  classifier.addSong('paperBag',
     ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'], 2);
-  classifier.songList.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2);
-  classifier.songList.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2);
+  classifier.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2);
+  classifier.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2);
   classifier.trainAll();
 
   it('sets welcome message', () => {
